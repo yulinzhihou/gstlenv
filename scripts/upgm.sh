7d4172f2304e907f81d17b55734a0e7faf2d46e5
yulinzhihou <yulinzhihou@163.com>
2021-09-16 22:04:55 +0800

    9-16 增加命令网站上传功能

--- scripts/upgm.sh
+++ scripts/upgm.sh
@@ -2,9 +2,9 @@
 # Author: yulinzhihou <yulinzhihou@gmail.com>
 # Forum:  https://gsgamesahre.com
 # Project: https://github.com/yulinzhihou/gs_tl_env.git
-# Date :  2021-02-01
+# Date :  2021-09-16
 # Notes:  GS_TL_Env for CentOS/RedHat 7+ Debian 10+ and Ubuntu 18+
-# comment: 用来配置在线GM网站访问。默认网站内容请自觉上传到/tlgame/www/gsgm目录下
+# comment: 用来配置在线GM网站访问。默认网站内容请自觉上传到/tlgame/www/gm目录下
 # 引入全局参数
 if [ -f /root/.gs/.env ]; then
   . /root/.gs/.env
@@ -17,3 +17,135 @@ if [ -f ./color.sh ]; then
 else
   . /usr/local/bin/color
 fi
+
+
+# 获取用户目录
+function getUserInput() {
+  # 配置是游戏注册还是登录器注册
+  while :; do echo
+      read -e -p "当前【域名】为${CBLUE}["0.0.0.0"]${CEND}，是否需要修改【0.0.0.0=使用服务器外网IP+端口访问】 [y/n](默认: n): " IS_MODIFY
+      IS_MODIFY=${IS_MODIFY:-'n'}
+      if [[ ! ${IS_MODIFY} =~ ^[y,n]$ ]]; then
+          echo "${CWARNING}输入错误! 请输入 y 或者 n ${CEND}"
+      else
+          if [ "${IS_MODIFY}" == 'y' ]; then
+              while :; do echo
+                  read -p "请输入【IP地址或者解析过来的域名】(默认: [0.0.0.0]): " DOMAIN_IP
+                  DOMAIN_IP=${DOMAIN_IP:-"0.0.0.0"}
+                  if [ ! -n ${DOMAIN_IP} ]; then
+                      sed -i "s/server_name  .*/server_name  ${DOMAIN_IP}/g" /tlgame/conf.d/ow.conf
+                      break
+                  else
+                      echo "${CWARNING}输入错误!${CEND}"
+                  fi
+              done
+          fi
+          break;
+      fi
+  done
+
+}
+
+# 创建目录，生成配置文件 
+function owConf() {
+  config=<<EOF
+server {
+    listen       80  default;
+    server_name  0.0.0.0;
+    root   /www/gm/public;
+    index  index.php index.html index.htm;
+    #charset koi8-r;
+    
+    access_log /dev/null;
+    #access_log  /var/log/nginx/nginx.localhost.access.log  main;
+    error_log  /var/log/nginx/nginx.localhost.error.log  warn;
+    
+    #error_page  404              /404.html;
+
+    # redirect server error pages to the static page /50x.html
+    #
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+
+    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+    #
+    #location ~ \.php$ {
+    #    proxy_pass   http://127.0.0.1;
+    #}
+
+    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+    #
+    location ~ \.php$ {
+        fastcgi_pass   gsphp:9000;
+        include        fastcgi-php.conf;
+        include        fastcgi_params;
+    }
+
+    # deny access to .htaccess files, if Apache's document root
+    # concurs with nginx's one
+    #
+    #location ~ /\.ht {
+    #    deny  all;
+    #}
+}
+
+# server {
+#     listen 443  default ssl http2;
+#     server_name  0.0.0.0;
+#     root   /www/ow;
+#     index  index.php index.html index.htm;
+#     #charset koi8-r;
+
+#     access_log /dev/null;
+#     #access_log  /var/log/nginx/nginx.localhost.access.log  main;
+#     error_log  /var/log/nginx/nginx.localhost.error.log  warn;
+
+#     #error_page  404              /404.html;
+
+#     ssl_certificate /ssl/localhost/localhost.crt;
+#     ssl_certificate_key /ssl/localhost/localhost.key;
+
+#     # redirect server error pages to the static page /50x.html
+#     #
+#     error_page   500 502 503 504  /50x.html;
+#     location = /50x.html {
+#         root   /usr/share/nginx/html;
+#     }
+
+#     # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+#     #
+#     #location ~ \.php$ {
+#     #    proxy_pass   http://127.0.0.1;
+#     #}
+
+#     # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+#     #
+#     location ~ \.php$ {
+#         fastcgi_pass   gsphp:9000;
+#         include        fastcgi-php.conf;
+#         include        fastcgi_params;
+#     }
+
+#     # deny access to .htaccess files, if Apache's document root
+#     # concurs with nginx's one
+#     #
+#     #location ~ /\.ht {
+#     #    deny  all;
+#     #}
+# }
+EOF
+
+  # 创建目录
+  if [ ! -d "/tlgame/www/gm" ]; then
+      mkdir -p /tlgame/www/gm
+      #
+      if [ ! -f /tlgame/conf.d/gm.conf ]; then
+          echo -e $config > /tlgame/conf.d/gm.conf
+      fi
+  fi
+}
+
+owConf
+getUserInput
