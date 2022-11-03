6d4e8749dad0acb8a5fb69b3d049de627924f70f
yulinzhihou <yulinzhihou@163.com>
2022-11-03 21:11:10 +0800

    调试 setpoint gsset 命令-5

--- include/update_tlbbdb_point.sql
+++ include/update_tlbbdb_point.sql
@@ -1,5 +1,1 @@
-UPDATE `tlbbdb`.`t_char` 
-SET POINT_PARAM = POINT_NUM 
-WHERE
-	aid IN ( SELECT aid FROM ( SELECT aid FROM t_char WHERE accname = ACCNAME LIMIT 1 OFFSET USER_INDEX ) AS aids );
-	
+UPDATE `tlbbdb`.`t_char` SET POINT_PARAM = POINT_NUM WHERE aid IN ( SELECT aid FROM ( SELECT aid FROM t_char WHERE accname = ACCNAME LIMIT 1 OFFSET USER_INDEX ) AS aids );
