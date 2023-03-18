b1dc9f1e279c41c39816308b2713de5d04b53494
yulinzhihou <yulinzhihou@163.com>
2023-03-18 16:27:04 +0800

    修复-docker-compose安装不成功的问题

--- install.sh
+++ install.sh
@@ -89,7 +89,8 @@ EOF
     fi
 
     if [ ! -f /usr/local/bin/docker-compose ]; then
-        curl -L https://get.daocloud.io/docker/compose/releases/download/${DOCKER_COMPOSER_VERSION:-v2.12.2}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+        # curl -L https://get.daocloud.io/docker/compose/releases/download/${DOCKER_COMPOSER_VERSION:-v2.12.2}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+        curl -L https://ghproxy.com/https://github.com/docker/compose/releases/download/${DOCKER_COMPOSER_VERSION:-v2.12.2}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
         chmod +x /usr/local/bin/docker-compose
     fi
     docker-compose --version >&/dev/null
