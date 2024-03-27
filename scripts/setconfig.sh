3f9a9fb7d80f55bb51b3386de979a1e512b39516
yulinzhihou <yulinzhihou@gmail.com>
2024-03-27 17:46:03 +0800

    集成php,redis镜像

--- scripts/setconfig.sh
+++ scripts/setconfig.sh
@@ -250,8 +250,8 @@ setconfig_rebuild() {
         exit 1
     fi
     # 先停止容器，再将容器删除，重新根据镜像文件以及配置文件，通过docker-compose重新生成容器环境
-    docker stop gsmysql gsnginx gsserver &&
-        docker rm -f gsmysql gsnginx gsserver &&
+    docker stop gsmysql gsnginx gsserver gsphp gsredis &&
+        docker rm -f gsmysql gsnginx gsserver gsphp gsredis &&
         rm -rf /tlgame/gsmysql/mysql
 }
 
